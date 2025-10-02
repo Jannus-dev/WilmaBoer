32ba705c294583b69b2fd00bce38b02825163583
Jannus_Dev <jan.honing2@gmail.com>
2025-10-02 13:44:54 +0200

    Header

---
+++ app/components/headerComponent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="header w-screen h-15 border-b-2 border-b-gray-400 rounded-b-2xl flex flex-row justify-center items-center">
+    <img src="../assets/img/Logo.svg" alt="Logo" class="h-12">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
