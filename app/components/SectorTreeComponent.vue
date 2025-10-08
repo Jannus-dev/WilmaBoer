4c7e55b464a2dd8eaaf04df8cad4804af7f5126f
Jannus_Dev <jan.honing2@gmail.com>
2025-10-08 13:56:55 +0200

    End of day

---
+++ app/components/SectorTreeComponent.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="SectorOne flex">
+    <img src="../assets/img/GreenWindow.jpg" class="h-screen"></img>
+    <div class="text ml-35 mt-15">
+      <TitleComponent text="Excelleren als interim" tag="h1" />
+      <TextComponent>
+        Ik heb mijn kennis en ervaring samengebracht in mijn high end aanbod voor tijdelijk leiders. <br>
+        Als mentor zorg ik er samen met jou voor dat: <br>
+        <br>
+          Jij gaat excelleren in je opdracht<br>
+          Jij claimt je expertstatus<br>
+          Dat jij zorgt voor impact en goede resultaten<br>
+        <br>
+        Vragen waar ik je bij ga ondersteunen zijn gaan over: <br>
+        <br>
+          Positionering (in-en extern)<br>
+          Strategie<br>
+          Inhoud<br>
+          Persoonlijk leiderschap<br>
+        <br>
+        Hoe zorg ervoor dat jij bij de 10% hoort en echt voor jou premium opdrachten binnenhaalt? <br>
+        <br>
+        Alle ervaringen en vaak zien dat tijdelijk leiders onbewust bekwaam het interim schap vormgeven, vastlopen in opdrachten en veel energie kwijt zijn om hun oplossingen uit te voeren. En voor je het weet rol je weer in eenzelfde opdracht.<br>
+        <br>
+        Ik zorg ervoor dat je regie krijgt over je vervolgopdracht<br>
+        Ik zorg ervoor dat je leiderschap hebt tijdens je opdracht<br>
+        Dat je helder hebt hoe je een opdracht vorm kan geven<br>
+        Hoe je helder hebt wat jouw expert status is<br>
+        <br>
+        Dat je weet welke waarden voor jou belangrijk zijn. Zodat je wat jij nodig hebt in je rol als tijdelijk leider. Zodat je goede energiebalans houdt.<br>
+      </TextComponent>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
