79a3f226ffa502fe037db54164124ae1b4a8e4bd
Jannus_Dev <jan.honing2@gmail.com>
2025-10-09 14:08:01 +0200

    Login pagina uiterlijk (Form)

---
+++ app/pages/login.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img src="../assets/img/Logo.svg" alt="Your Company" class="mx-auto h-50 w-auto" />
+      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-paars">Log in voor toegang van dashboard</h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form action="#" method="POST" class="space-y-6">
+        <div>
+          <label for="email" class="block text-sm/6 font-medium text-paars">Email address</label>
+          <div class="mt-2">
+            <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm/6 font-medium text-paars">Password</label>
+          </div>
+          <div class="mt-2">
+            <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-LichtPaars px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-Goud focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
