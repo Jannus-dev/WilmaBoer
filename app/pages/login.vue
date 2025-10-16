9054e4e794e508eee3a218bdc7a94645406f64f2
Jannus_Dev <jan.honing2@gmail.com>
2025-10-16 15:05:37 +0200

    Db conection fix

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,6 +1,31 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useNuxtApp } from '#app'
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import {NuxtLink} from "#components";
+import dashboard from "~/pages/dashboard.vue";
+import Dashboard from "~/pages/dashboard.vue";
 
+const email = ref('')
+const password = ref('')
+const error = ref('')
+
+const { $auth } = useNuxtApp()  // auth uit plugin
+
+const login = async () => {
+  error.value = ''
+  try {
+    const userCredential = await signInWithEmailAndPassword($auth, email.value, password.value)
+    console.log('Logged in:', userCredential.user)
+    // redirect naar dashboard
+    navigateTo('/dashboard')
+  } catch (err: any) {
+    error.value = err.message
+    console.error(err)
+  }
+}
 </script>
+
 
 <template>
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -10,11 +35,11 @@
     </div>
 
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form action="#" method="POST" class="space-y-6">
+      <form @submit.prevent="login" action="#" method="POST" class="space-y-6">
         <div>
           <label for="email" class="block text-sm/6 font-medium text-paars">Email address</label>
           <div class="mt-2">
-            <input id="email" type="email" name="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
+            <input id="email" type="email" name="email" v-model="email" required autocomplete="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
           </div>
         </div>
 
@@ -23,9 +48,11 @@
             <label for="password" class="block text-sm/6 font-medium text-paars">Password</label>
           </div>
           <div class="mt-2">
-            <input id="password" type="password" name="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
+            <input id="password" type="password" name="password" v-model="password" required autocomplete="current-password" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-CickRood sm:text-sm/6" />
           </div>
         </div>
+
+        <p v-if="error" class="text-red-500">{{ error }}</p>
 
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-LichtPaars px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-Goud focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
--- app/plugins/firebase.client.ts
+++ app/plugins/firebase.client.ts
@@ -3,17 +3,16 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 export default defineNuxtPlugin(() => {
-    const firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_PROJECT_ID.appspot.com",
-        messagingSenderId: "YOUR_SENDER_ID",
-        appId: "YOUR_APP_ID"
+    const config = {
+        apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+        authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+        projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+        storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+        appId: import.meta.env.VITE_FIREBASE_APP_ID
     }
 
-    const app = initializeApp(firebaseConfig)
-
+    const app = initializeApp(config)
     const auth = getAuth(app)
     const db = getFirestore(app)
 
